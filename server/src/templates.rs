0c9f37737458147cc040c09fc50be7f8b1c83823
Andrei <andrei.i@posteo.de>
2025-12-16 00:00:00 +0000

    Move style from doc.html

--- server/src/templates.rs
+++ server/src/templates.rs
@@ -126,7 +126,7 @@ pub fn investigate_link(payload: &String) -> Safe<String> {
 }
 
 pub fn external_link(link: &str, title: &str) -> Safe<String> {
-    let title = format!("{title}↗");
+    let title = format!("{title}<svg width=\"13.5\" height=\"13.5\" aria-hidden=\"true\" viewBox=\"0 0 24 24\"><path fill=\"currentColor\" d=\"M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z\"></path></svg>");
     Safe(
         HtmlElement::new(HtmlTag::Link)
             .with_attribute("href", link)
